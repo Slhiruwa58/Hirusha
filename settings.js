805f583698a8919e44e3abb5cdeec89a38a7654c
Slhiruwa58 <Deshanhiru111@gmail.com>
2025-01-30 13:33:04 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Hirusha ', // Your name
+  botOwner: 'Hirusha', // Your name
   ownerNumber: '94786669785', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
